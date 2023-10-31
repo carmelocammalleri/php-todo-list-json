663f33912802826ccf16957240cd893863be1f68
Melozzo <carmecamma1995@gmail.com>
2023-10-31 10:40:40 +0100

    [linked]: file json with server.php

---
+++ server.php
@@ -0,0 +1,6 @@
+<?php
+
+$str_json = file_get_contents('todoList.json');
+
+// il var dump legge dentro il file server.php quindi nel local host bisogna inserire il nome del server per vederlo
+var_dump($str_json);
